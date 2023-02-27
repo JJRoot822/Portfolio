7ff493aa3de9218b30a01d004e7c29b3739f5bd5
JJRoo822 <josh.root822@gmail.com>
2023-02-27 03:59:33 -0800

    Modified the User model and added the necessary reference to the connection string.

--- ASP.NET Core Web API/RetailManagerAPI/RMDataAccess/Models/User.cs
+++ ASP.NET Core Web API/RetailManagerAPI/RMDataAccess/Models/User.cs
@@ -4,20 +4,31 @@ using Microsoft.EntityFrameworkCore;
 namespace RMDataAccess.Models
 {
     [PrimaryKey("UserID")]
-    class User
+    public class User
     {
         public int UserID { get; set; }
-        
+
+        [Required]
+        [MaxLength(75)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string LastName { get; set; }
+
+        [MaxLength(1)]
+        public string? MiddleInitial { get; set; }
+
         [Required]
         [MaxLength(20)]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
         [Required]
         [MaxLength(150)]
-        public string? EmailAddress { get; set; }
+        public string EmailAddress { get; set; }
 
         [Required]
         [MaxLength(256)]
-        public string? Password { get; set; }
+        public string Password { get; set; }
     }
 }
--- ASP.NET Core Web API/RetailManagerAPI/RMDataAccess/UserContext.cs
+++ ASP.NET Core Web API/RetailManagerAPI/RMDataAccess/UserContext.cs
@@ -1,20 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RMDataAccess.Models;
 
 namespace RMDataAccess
 {
-    class UserContext : DbContext
+    public class UserContext : DbContext
     {
         public DbSet<User> users { get; set; }
 
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
+            
+        }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL(DBUtil.AuthConnectionString);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
